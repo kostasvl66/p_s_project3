3cea8019ae49ec9315646849f80d4a9cb6045ed5
Kostas <kostasvl66@gmail.com>
2026-01-10 20:21:27 +0200

    Minor Changes

--- 3.2/main.c
+++ 3.2/main.c
@@ -80,6 +80,8 @@ int main(int argc, char *argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
+    printf("Processes:%d\n", comm_sz);
+
     /* -------------------- Matrix/vector allocation -------------------- */
 
     if (my_rank == 0) {
